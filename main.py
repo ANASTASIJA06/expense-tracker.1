8d50687750900cefbc3f02a82f4849f33529f88f
Anastasija Midlbruka <150901546+ANASTASIJA06@users.noreply.github.com>
2024-02-14 13:18:35 +0200

    1 main.py

--- main.py
+++ main.py
@@ -5,6 +5,15 @@
 #  |_____/_/\_\ .__/ \___|_| |_|___/\___||___/   |_||_|  \__,_|\___|_|\_\___|_|   
 #             |_|                                                                 
 
+# Задача:
+# Напишите программу, которая позволяет
+# - вводить расходы: название, сумму и категорию
+# - отображать все расходы на экране
+# - возможность выбрать 10 больших расходов
+# - возможность выбрать 10 меньших расходов
+# - возможность просматривать среднюю сумму расходов
+# [!] Программа сохраняет статус расходов при выключении и перезапуске программы
+
 #
 # Uzdevums:
 # Uzrakstīt programmu, kas ļauj
@@ -15,22 +24,43 @@
 # - iespēja apskatīt vidējo izdevumu summu
 # [!] Programmai jaglabā izdevumu stāvokli kad programma ir izslēgta palaista no jauna
 #
+import json
 
-expenses = []
+with open("expenses.json", "r") as outfile:
+    expenses = json.load(outfile)
 
 # load expenses from expenses.json file here
 # https://www.geeksforgeeks.org/read-write-and-parse-json-using-python/ (Python read JSON file)
-pass
+
+def sum_() 
 
 while True:
     command = input("\nChoose command:")
     if command == "1":
-        pass
+        name = input('Enter the name: ')
+        amount = int(input('Enter the amount: '))
+        category = input('Enter the category: ')
+        expense = {
+            'Name': name,
+            'Amount': amount,
+            'Category': category
+           }
+        expenses.append(expense)
+        print(expenses)
+        print("===============") 
+    elif command == "2":
+        print(expenses)
+        print("===============")   
+        
+    elif command == "3":
+
+        print("===============")   
     if command == "e":
         print("Exiting...")
         break
 
 # save expenses to expenses.json file here
 # https://www.geeksforgeeks.org/read-write-and-parse-json-using-python/ (Writing JSON to a file in Python)
-pass
 
+with open("expenses.json", "w") as outfile:
+    json.dump(expenses, outfile)
